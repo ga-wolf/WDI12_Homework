81689c8a3c8d91ab609b7d5591c8b5dd7e315b01
Timothy Tsui <timiscoding@gmail.com>
2015-12-03 23:18:07 +1100

    Add and delete records

--- TimothyTsui/week_04/breaking_bad/main.rb
+++ TimothyTsui/week_04/breaking_bad/main.rb
@@ -3,6 +3,19 @@ require 'sinatra/reloader'
 require 'sqlite3'
 require 'pry'
 
+# show the add character form
+get '/characters/add' do
+  erb :character_add
+end
+
+# add character to database
+post '/characters/add' do
+  query = "INSERT INTO characters (first_name, last_name, age, gender, occupation, aliases, image) VALUES ('#{ params[:first_name] }', '#{ params[:last_name] }', #{ params[:age] }, '#{ params[:gender] }', '#{ params[:occupation] }', '#{ params[:aliases] }', '#{ params[:image] }')"
+  query_db(query)
+  redirect to('/characters')
+end
+
+# get all characters
 get '/characters' do
   query = 'SELECT * FROM characters'
   @characters = query_db(query)
@@ -11,11 +24,19 @@ get '/characters' do
   erb :characters
 end
 
+# get a character
 get '/characters/:id' do
   query = "SELECT * FROM characters WHERE id=#{ params[:id] }"
   @character = query_db(query).first
   erb :character_show
 end
+
+get '/characters/:id/delete' do
+  query = "DELETE FROM characters WHERE id=#{ params[:id] }"
+  query_db(query)
+  redirect to('/characters')
+end
+
 
 def query_db(sql)
   db = SQLite3::Database.new('characters.db')

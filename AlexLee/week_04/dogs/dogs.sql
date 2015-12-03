eace3e3307ab1edd313082cf3fa9331c6af4e90d
Alex Lee <alexwlee.web@gmail.com>
2015-12-04 09:34:56 +1100

    Submitting database homework

---
+++ AlexLee/week_04/dogs/dogs.sql
@@ -0,0 +1,16 @@
+CREATE TABLE dogs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    breed TEXT,
+    image TEXT
+);
+
+INSERT INTO dogs (breed, image) VALUES ("Border Collie", "http://content.sitezoogle.com.s3.amazonaws.com/u/57327/style/page-photo-12513.png?e16e507e75bb12a1b677268d2f3886eb");
+INSERT INTO dogs (breed, image) VALUES ("Golden Retriever", "https://i.ytimg.com/vi/2EfHtzauHcM/maxresdefault.jpg");
+INSERT INTO dogs (breed, image) VALUES ("Labrador", "http://images.nationalgeographic.com/wpf/media-live/photos/000/347/cache/golden-labrador-puppy_34708_990x742.jpg");
+INSERT INTO dogs (breed, image) VALUES ("Lhasa Apso", "http://i.imgur.com/SZWJJtY.jpg");
+INSERT INTO dogs (breed, image) VALUES ("Maltese", "http://malteze.net/images/sampledata/maltez/1po_6574848565644857.jpg");
+INSERT INTO dogs (breed, image) VALUES ("Poodle", "http://www.memobee.com/images.php?param=content_type=image&fs=-1&file=chairil729@gmail.com/images/writings/0207101113.jpg");
+INSERT INTO dogs (breed, image) VALUES ("Shih Tzu", "http://buzzsharer.com/wp-content/uploads/2015/09/awesome-eyes-face-shih-tzu.jpg");
+INSERT INTO dogs (breed, image) VALUES ("Terrier", "http://hdwpics.com/images/0E275AC5C52E/Yorkshire-Terrier-puppy.jpg");
+INSERT INTO dogs (breed, image) VALUES ("St. Bernard", "http://eskipaper.com/images/saint-bernards-4.jpg");
+

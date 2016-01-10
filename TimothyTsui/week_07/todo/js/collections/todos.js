e678f65d1ed3e73d9964861ab5fffb2a65255c41
Timothy Tsui <timiscoding@gmail.com>
2016-01-10 15:22:29 +1100

    todo app fri hw

---
+++ TimothyTsui/week_07/todo/js/collections/todos.js
@@ -0,0 +1,43 @@
+var app = app || {};
+
+var TodoList = Backbone.Collection.extend({
+  model: app.Todo,
+
+  // save all todo items under todos-backbone namespace
+  localStorage: new Backbone.LocalStorage('todos-backbone'),
+
+  // get all completed items
+  completed: function() {
+    return this.filter(function( todo ) {
+      return todo.get('completed');
+    });
+  },
+
+  // get incomplete items
+  remaining: function() {
+    return this.without.apply( this, this.completed() );
+  },
+
+  // sequence generator
+  nextOrder: function() {
+    if ( !this.length ) {
+      return 1;
+    }
+    return this.last().get('order') + 1;
+  },
+
+  // adds items sorted by 'order'
+  comparator: function( todo ) {
+    return todo.get('order');
+  }
+});
+
+app.Todos = new TodoList();
+
+// var list = new TodoList([
+//     {title: 'do 1', completed: false},
+//     {title: 'do 2', completed: true}
+// ]);
+
+// console.log(list.first().toJSON(), list.at(1).toJSON());
+// console.log(list.remaining()[0].toJSON());

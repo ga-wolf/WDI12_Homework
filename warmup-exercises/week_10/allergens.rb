7a1ad1915d8900b82fefbb4ab198b2c8d86742d2
anonymous-wolf <jackjeffress@gmail.com>
2016-01-29 10:36:29 +1100

    Added allergies warmup

---
+++ warmup-exercises/week_10/allergens.rb
@@ -0,0 +1,37 @@
+require 'pry'
+class Allergies
+
+  ALLERGENS = {
+    1 => "eggs",
+    2 => "peanuts",
+    4 => "shellfish",
+    8 => "strawberries",
+    16 => "tomatoes",
+    32 => "chocolate",
+    64 => "pollen",
+    128 => "cats",
+  }
+
+  def initialize( score )
+    @score = score
+  end
+
+  def list
+    allergies = []
+    ALLERGENS.each do |bitflag, allergen|
+      if ( @score & bitflag ) > 0
+        allergies.push allergen
+      end
+    end
+    allergies
+  end
+
+  def allergic_to? allergen
+    list.include? allergen
+  end
+
+end
+
+a = Allergies.new( 34 )
+puts a.list
+puts a.allergic_to? "peanuts"
